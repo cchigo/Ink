47f6ee19cc0d9d96d9cd1f79ce44a9fbde4a0046
cchigo <ccasadu15@gmail.com>
2019-12-30 15:23:29 +0100

    create Blog Adapter

--- app/src/main/java/com/example/ink/blogList/BlogAdapter.kt
+++
@@ -1,6 +0,0 @@
-package com.example.ink.blogList
-
-import androidx.recyclerview.widget.RecyclerView
-
-class BlogAdapter{
-}
---
+++ app/src/main/java/com/example/ink/ui/blogList/BlogAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.ink.ui.blogList
+
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ink.utils.OnItemClickListener
+
+class BlogAdapter (val onItemClickListener: OnItemClickListener, val context: Context): RecyclerView.Adapter<BlogAdapter.BlogViewHolder>() {
+
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: BlogAdapter.BlogViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class BlogViewHolder(){
+
+    }
+}
