3438a2630906204b8f23bf848560b289127a037e
cchigo <ccasadu15@gmail.com>
2020-01-01 16:21:25 +0100

    set toolbar style

--- app/src/main/java/com/example/ink/ui/addBlogpost/AddBlogpost.kt
+++ app/src/main/java/com/example/ink/ui/addBlogpost/AddBlogpost.kt
@@ -3,9 +3,14 @@ package com.example.ink.ui.addBlogpost
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.ViewModelProviders
 import com.example.ink.R
+import com.example.ink.databinding.ActivityAddBlogpostBinding
+import com.example.ink.ui.blogList.BlogViewModel
 import kotlinx.android.synthetic.main.activity_add_blogpost.*
 
 
@@ -19,12 +24,27 @@ class AddBlogpost : AppCompatActivity() {
         var toolbar: Toolbar = toolbar
         setSupportActionBar(toolbar)
 
+        var binding = ActivityAddBlogpostBinding.inflate(layoutInflater)
+        val model = ViewModelProviders.of(this)[BlogViewModel::class.java]
+
+
 
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.toolbar_menu, menu)
         return true
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        var id: Int = item.itemId
+
+        if(id == R.id.postButton_id){
+            Toast.makeText(applicationContext, "you clicked send", Toast.LENGTH_LONG).show()
+        }
+        return true
+    }
+
 
 }
 
